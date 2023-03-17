44a1468054d0ea66156413bf89ec0eae0d1ffb1d
moukengue aline <aline.moukengue80@gmail.com>
2023-03-17 11:51:26 +0100

    first commit

---
+++ artists.php
@@ -0,0 +1,41 @@
+<?php 
+
+ include "db.php";
+ 
+$db = connexionBase();
+$requete = $db->query("SELECT * FROM artist");
+$tableau = $requete->fetchAll(PDO::FETCH_OBJ);
+$requete->closeCursor();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO - Liste</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nom</th>
+        </tr>
+
+        <?php foreach ($tableau as $artist): ?>
+
+        
+        <tr>
+            <td><?= $artist->artist_id ?></td>
+            <td><?= $artist->artist_name ?></td>
+            <td><a href="artist_detail.php?id=<?= $artist->artist_id ?>">Détail</a></td>
+        </tr>
+
+        <?php endforeach; ?>
+
+    </table>
+</body>
+</html>
